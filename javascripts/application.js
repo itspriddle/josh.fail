4a05eb3c68ce03e788fef5705dba197550e8e9d0
Joshua Priddle <jpriddle@nevercraft.net>
2010-02-28 01:27:49 -0500

    Try getJSON

--- javascripts/application.js
+++ javascripts/application.js
@@ -1,14 +1,10 @@
 jQuery(document).ready(function($) {
   $('#testing').click(function() {
     alert('clicked testing');
-    $.ajax({
-      url: '/javascripts/posts.json',
-      dataType: 'json',
-      success: function(data) {
-        $.each(data.posts, function (i, item) {
-          alert(item.title);
-        });
-      }
+    $.getJSON('/javascripts/posts.json', function(data) {
+      $.each(data.posts, function (i, item) {
+        alert(item.title);
+      });
     });
   });
 });
