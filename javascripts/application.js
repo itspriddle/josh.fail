73c2445befcb54882a0d1bdad33d4c2438b73e69
Joshua Priddle <jpriddle@nevercraft.net>
2010-02-28 00:43:37 -0500

    testing js

--- javascripts/application.js
+++ javascripts/application.js
@@ -1,9 +1,9 @@
 $(document).ready(function() {
   // Stuff to do as soon as the DOM is ready;
+  $('#testing').click(
+    alert('clicked testing');
+    $.getJSON('/javascripts/posts.json', function(data) {
+      alert(data);
+    });
+  );
 });
-
-function testing() {
-  $.getJSON('/javascripts/posts.json', function(data) {
-    alert(data);
-  });
-}
