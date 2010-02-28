2f669180bb918ccac63804fca298bfe4c94b18c7
Joshua Priddle <jpriddle@nevercraft.net>
2010-02-28 01:25:40 -0500

    Fix js

--- javascripts/application.js
+++ javascripts/application.js
@@ -1,10 +1,14 @@
 jQuery(document).ready(function($) {
-  $('#testing').click(
+  $('#testing').click(function() {
     alert('clicked testing');
-    $.getJSON('/javascripts/posts.js', function(data) {
-      $.each(data.posts, function (i, item) {
-        alert(item.title);
-      });
+    $.ajax({
+      url: '/javascripts/posts.json',
+      dataType: 'json',
+      success: function(data) {
+        $.each(data.posts, function (i, item) {
+          alert(item.title);
+        });
+      }
     });
-  );
+  });
 });
