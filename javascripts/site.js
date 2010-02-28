541f9a88b8f0423a5866c7155d5f02c36a7a9baa
Joshua Priddle <jpriddle@nevercraft.net>
2010-02-28 00:34:24 -0500

    Change to json

--- javascripts/site.js
+++
@@ -1,9 +0,0 @@
----
-layout: nil
----
-var Site = {
-  posts: []
-};
-
-{% for post in site.posts %}Site.posts.push({date: '{{ post.date | date: "%Y-%m-%d" }}', title: '{{ post.title }}', url: '{{ post.url }}'});
-{% endfor %}
