6d3f6acb963945f907760e99c875e0e7e84b04a8
Joshua Priddle <jpriddle@nevercraft.net>
2010-02-28 01:46:25 -0500

    Testing sidebar

--- javascripts/application.js
+++ javascripts/application.js
@@ -1,10 +1,11 @@
 jQuery(document).ready(function($) {
-  $('#testing').click(function() {
-    alert('clicked testing');
-    $.getJSON('/javascripts/posts.json', function(data) {
-      $.each(data.posts, function (i, item) {
-        alert(item.title);
-      });
-    });
-  });
+  $('input#searchfield').autocomplete('/javascripts/posts.json');
+  // $('#testing').click(function() {
+  //   alert('clicked testing');
+  //   $.getJSON('/javascripts/posts.json', function(data) {
+  //     $.each(data.posts, function (i, item) {
+  //       alert(item.title);
+  //     });
+  //   });
+  // });
 });
